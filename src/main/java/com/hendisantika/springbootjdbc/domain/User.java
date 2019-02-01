f3766b06d0aacbb49e9354f99c77098ebd02c0a2
Hendi Santika <hendisantika@gmail.com>
2019-02-01 07:25:35 +0700

    #3 - Add User Domain

---
+++ src/main/java/com/hendisantika/springbootjdbc/domain/User.java
@@ -0,0 +1,51 @@
+package com.hendisantika.springbootjdbc.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-jdbc
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-02-01
+ * Time: 07:25
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+    private int id;
+    private String name;
+    private String address;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
